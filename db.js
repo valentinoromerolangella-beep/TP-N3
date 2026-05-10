749c65dfdcafc40530f07f20eb07d01134b5a759
valentinoromerolangella-beep <valentinoromerolangella@gmail.com>
2026-05-10 02:19:06 -0300

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/gestion_gimnasio',
-});
-
-module.exports = pool;
